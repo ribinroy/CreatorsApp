478609fdbed881ef4df7acab66b22aebad65d130
Ribin <ribinroy4@gmail.com>
2020-08-08 19:21:41 +0530

    Card details popup competed

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -152,9 +152,9 @@ function PaymentPopUp(props){
                     }
                     <p>You payment is safe and secured by Stripe</p>
                     <div className="linksWrap">
-                        <a href="https://www.google.com/" target="_blank" className="link">Contact</a>
-                        <a href="https://www.google.com/" target="_blank" className="link">Refund policy</a>
-                        <a href="https://www.google.com/" target="_blank" className="link">Terms and Conditions</a>
+                        <a href="https://www.google.com/" rel="noopener noreferrer" target="_blank" className="link">Contact</a>
+                        <a href="https://www.google.com/" rel="noopener noreferrer" target="_blank" className="link">Refund policy</a>
+                        <a href="https://www.google.com/" rel="noopener noreferrer" target="_blank" className="link">Terms and Conditions</a>
                     </div>
                 </div>
             </div>
